51871527f43d67b562136bd1545f28d2003bbb3f
College-Media <collegemedia.presidency@gmail.com>
2024-10-24 14:48:01 +0530

    Uploaded some file

---
+++ college_media/college_media/assets/js/home.js
@@ -0,0 +1,8 @@
+function fun1() {
+  var menuTable = document.querySelector("#post-menu-table"); 
+    menuTable.style.display = "block";
+  }
+function fun() { 
+  var cancel = document.getElementById("post-menu-table"); 
+    cancel.style.display = "none";
+  }
