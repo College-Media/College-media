cdb86ec89572f268391868f3ba212ea4ed92bf0e
Aradhyshetty <shettyaradhya24@gmail.com>
2024-10-24 11:41:10 +0530

    trying to changes the main page to load

--- college_media/college_media/college_media/urls.py
+++ college_media/college_media/college_media/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+import college_media.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name="home"),
     path("addmin_dash/",include('admin_app.urls')),
     path("staff_dash/",include('staff_app.urls')),
     path("user_dash/",include('user_app.urls'))
