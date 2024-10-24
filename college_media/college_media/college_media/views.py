7617e03a921010331a8a82cbd302ab1f58263bf2
Aradhyshetty <shettyaradhya24@gmail.com>
2024-10-24 15:26:39 +0530

    email sending
    written code in login page to send a email

--- college_media/college_media/college_media/views.py
+++ college_media/college_media/college_media/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def home(request):
+    subject = 'Welcome to our website'
+    message = 'Thank you for registering at our site.'
+    recipient_list = ['aradhyashetty74@gmail.com']  # The recipient’s email
+    email_from = settings.DEFAULT_FROM_EMAIL
+    
+    send_mail(subject, message, email_from, recipient_list)
     return render(request,"login.html")
