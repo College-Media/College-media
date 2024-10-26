49172d8dcab73de6e27a46fd6b7cb4c8f347b290
College-Media <collegemedia.presidency@gmail.com>
2024-10-27 00:46:38 +0530

    added js and css for profile

--- college_media/college_media/assets/js/profile.js
+++ college_media/college_media/assets/js/profile.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.editBtn').addEventListener('click', function () {
         document.getElementById('editProfileForm').style.display = 'block';
         document.getElementById('overlay').style.display = 'block';
+        
     });
 
     document.getElementById('closeButton').addEventListener('click', function () {
@@ -12,13 +13,18 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('overlay').addEventListener('click', function () {
         document.getElementById('editProfileForm').style.display = 'none';
         document.getElementById('overlay').style.display = 'none';
+        document.getElementById('overlay2').style.display = 'none';
+        document.getElementById('profile-edit-pic-table').style.display='none';
+       
     });
     document.getElementById('profile-image-edit').addEventListener('click',function(){
-        
+        document.getElementById('overlay2').style.display = 'block';
         document.getElementById('profile-edit-pic-table').style.display='block';
-        document.getElementById('overlay').style.display = 'block';
+       
     });
     document.getElementById('cancelBtn').addEventListener('click',function(){
-        document.getElementById('overlay2').style.display = 'block';
+        document.getElementById('overlay2').style.display = 'none';
+        document.getElementById('profile-edit-pic-table').style.display='none';
+       
     });
 });
