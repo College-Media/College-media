34247ea84df5a6ba06fb6ebbd86dfba31ae2fe96
Aradhyshetty <shettyaradhya24@gmail.com>
2024-10-27 15:10:18 +0530

    minor changes

--- college_media/college_media/staff_app/urls.py
+++ college_media/college_media/staff_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from staff_app import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.welcome),
     path("add_student/",views.add_student,name="add_student"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
