5fe3cf917c872869f303343af7c0eaa9c97aea1f
Adithyamaiya <101737761+Adithyamaiya@users.noreply.github.com>
2024-10-27 14:58:35 +0530

    search page impemented for staff and student

--- college_media/college_media/staff_app/urls.py
+++ college_media/college_media/staff_app/urls.py
@@ -4,4 +4,5 @@ from staff_app import views
 urlpatterns = [
     path('', views.welcome),
     path("add_student/",views.add_student,name="add_student"),
+    path("home/",views.home,name="home")
 ]
