c67ff7fa8a33d1b8e04a82da8b0303ba2cd0d8d4
Aradhyshetty <shettyaradhya24@gmail.com>
2024-10-30 13:17:47 +0530

    arada

--- college_media/college_media/college_media/settings.py
+++ college_media/college_media/college_media/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'staff_app.context_processors.student_profile',
             ],
         },
     },
