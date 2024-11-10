530bc755266224e38982e8fcb0a864ccd95dacd6
College-Media <collegemedia.presidency@gmail.com>
2024-11-10 23:32:07 +0530

    profile page edit with css

--- college_media/college_media/assets/js/profile.js
+++ college_media/college_media/assets/js/profile.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.editBtn').addEventListener('click', function () {
+    document.querySelectorAll('.editBtn').forEach(button => {
+        button.addEventListener('click', function () {
         document.getElementById('editProfileForm').style.display = 'block';
         document.getElementById('overlay').style.display = 'block';
         
     });
-
+    });
     document.getElementById('closeButton').addEventListener('click', function () {
         document.getElementById('editProfileForm').style.display = 'none';
         document.getElementById('overlay').style.display = 'none';
