c805f44da4ed89a966a9b562b4f0b1cf8db61232
Adithyamaiya <101737761+Adithyamaiya@users.noreply.github.com>
2024-11-11 22:53:54 +0530

    showing search profile

--- college_media/college_media/user_app/models.py
+++ college_media/college_media/user_app/models.py
@@ -7,10 +7,14 @@ from staff_app.models import *
 
 class Post(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    
     content = models.TextField()
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.student.name
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
