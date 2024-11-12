4bbe9af6ba8b51647e4fb38686a371a809dbf4d7
College-Media <collegemedia.presidency@gmail.com>
2024-11-13 03:35:33 +0530

    post in profile

--- college_media/college_media/assets/js/profile.js
+++ college_media/college_media/assets/js/profile.js
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('overlay').style.display = 'none';
         document.getElementById('overlay2').style.display = 'none';
         document.getElementById('profile-edit-pic-table').style.display='none';
-       
     });
     document.getElementById('profile-in-edit').addEventListener('click',function(){
         document.getElementById('overlay2').style.display = 'block';
@@ -28,4 +27,32 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('profile-edit-pic-table').style.display='none';
        
     });
+    //for post
+    document.getElementById('approved-post').addEventListener('click',function(){
+        document.getElementById('editPostForm').style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+       
+    });
+    document.getElementById('closeButton').addEventListener('click', function () {
+        document.getElementById('editPostForm').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+    });
+    document.getElementById('overlay').addEventListener('click', function () {
+        document.getElementById('editPostForm').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+        document.getElementById('overlay2').style.display = 'none';
+        document.getElementById('post-edit-action-table').style.display='none';
+    });
+    document.getElementById('post-in-edit-actions').addEventListener('click',function(){
+        document.getElementById('overlay2').style.display = 'block';
+        document.getElementById('post-edit-action-table').style.display='block';
+       
+    });
+    document.getElementById('cancelBtn').addEventListener('click',function(){
+        document.getElementById('overlay2').style.display = 'none';
+        document.getElementById('post-edit-action-table').style.display='none';
+        document.getElementById('editPostForm').style.dispaly="none";
+        document.getElementById('overlay').style.display = 'none';
+    });
+
 });
