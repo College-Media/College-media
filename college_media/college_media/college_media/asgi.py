db8d3a78688336ac7b9ef1a51b91c7e6976feeb1
Adithyamaiya <101737761+Adithyamaiya@users.noreply.github.com>
2024-12-03 15:23:12 +0530

    Update asgi.py

--- college_media/college_media/college_media/asgi.py
+++ college_media/college_media/college_media/asgi.py
@@ -1,19 +1,14 @@
 import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat_app.routing  # Ensure you have `routing.py` in your `chat_app`
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_media.settings')
 
-# Initialize Django
-import django
-django.setup()
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat_app.routing
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": get_asgi_application(),  # For HTTP requests
+    "websocket": AuthMiddlewareStack(  # For WebSocket requests
         URLRouter(
             chat_app.routing.websocket_urlpatterns
         )
