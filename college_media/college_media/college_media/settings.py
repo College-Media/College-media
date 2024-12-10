8626ae4ce436f0e3b9afa11ce5e4132244ea0963
Adithyamaiya <101737761+Adithyamaiya@users.noreply.github.com>
2024-12-10 13:54:00 +0530

    small changes

--- college_media/college_media/college_media/settings.py
+++ college_media/college_media/college_media/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4dij^jsxy(@seg_7u_o$261jgg4*4y1+t5!anchnk2^5s+vsl$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.20.117']
 
 
 # Application definition
